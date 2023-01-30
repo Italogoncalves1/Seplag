b07c0c32f3b7addb6f4325a7914aa09abbfb28a8
Italo Gonçalves <101333657+Italogoncalves1@users.noreply.github.com>
2023-01-30 05:57:33 -0300

    Galelria create

---
+++ js/services/constants.js
@@ -0,0 +1,92 @@
+import img1 from "../../assets/galeria/img1.svg"
+import img2 from "../../assets/galeria/img2.svg"
+import img3 from "../../assets/galeria/3.svg"
+import img4 from "../../assets/galeria/img4.svg"
+import img5 from "../../assets/galeria/img5.svg"
+import img6 from "../../assets/galeria/img6.svg"
+import img7 from "../../assets/galeria/img7.svg"
+import img8 from "../../assets/galeria/img8.svg"
+import img9 from "../../assets/galeria/img9.svg"
+
+
+export const noticias =[
+    {
+        id: "1",
+        name: "Feriado á vista!",
+        date: "22/06/2022",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu molestie est, ac sollicitudin ligula. Curabitur nec enim et justo aliquam tristique ac eget orci. Quisque tempor neque nibh, pellentesque rutrum orci commodo quis. Curabitur volutpat erat et elit rutrum ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque laoreet sem nisi, in viverra metus euismod et. Aenean a eros id diam consequat sodales.",
+        autor: "Seplag"
+    },
+    {
+        id: "2",
+        name: "Novo curso Egal",
+        date: "20/06/2022",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu molestie est, ac sollicitudin ligula. Curabitur nec enim et justo aliquam tristique ac eget orci. Quisque tempor neque nibh, pellentesque rutrum orci commodo quis. Curabitur volutpat erat et elit rutrum ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque laoreet sem nisi, in viverra metus euismod et. Aenean a eros id diam consequat sodales.",
+        autor: "Seplag"
+    },
+    {
+        id: "3",
+        name: "Reunião Geral",
+        date: "20/06/2022",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu molestie est, ac sollicitudin ligula. Curabitur nec enim et justo aliquam tristique ac eget orci. Quisque tempor neque nibh, pellentesque rutrum orci commodo quis. Curabitur volutpat erat et elit rutrum ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque laoreet sem nisi, in viverra metus euismod et. Aenean a eros id diam consequat sodales.",
+        autor: "Seplag"
+    }
+];
+
+export const galeria = [
+    {
+      id: 1,
+      date:"Julho . 2022",
+      img: img1,
+      
+    },
+    {
+      id: 2,
+      date:"Julho . 2022",
+      img_url: img2,
+      
+    },
+    {
+      id: 3,
+      date:"Julho . 2022",
+      img_url: img3,
+      
+    },
+    {
+      id: 4,
+      date:"Julho . 2022",
+      img_url: img4,
+
+      
+    },
+    {
+      id: 5,
+      date:"Julho . 2022",
+      img_url: img5,
+      
+    },
+    {
+      id: 6,
+      date:"Julho . 2022",
+      img_url: img6,
+      
+    },
+    {
+      id: 7,
+      date:"Julho . 2022",
+      img_url: img7,
+      
+    },
+    {
+      id: 8,
+      date:"Julho . 2022",
+      img_url: img8,
+      
+    },
+    {
+      id: 9,
+      date:"Julho . 2022",
+      img_url: img9,
+      
+    },
+  ];
